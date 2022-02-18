7d4bfa35e68e1b46e5e68e95b7297195684fe6e6
Edward Edberg <edward.edberg.halim@gmail.com>
2022-02-17 22:46:55 -0800

    learn classes

--- script.js
+++ script.js
@@ -104,3 +104,33 @@ console.log(bmw, tesla);
 bmw.brake();
 tesla.brake();
 console.log(bmw, tesla);
+
+// class expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  // Method will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.jessica.greet();
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
